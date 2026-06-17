87740998bf8dfbddaf48573e8267a5bd09f41cc9
Amr <amrazribi1@gmail.com>
2026-06-18 00:43:44 +0400

    larp larp

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, addDoc, query, orderBy, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "larplarplarp.firebaseapp.com",
-    projectId: "larplarplarp",
-    storageBucket: "larplarplarp.appspot.com",
-    messagingSenderId: "428336369892",
-    appId: "1:428336369892:web:0c968b393686ccc9729cf1"
-};
+const firebaseConfig = { /* USE YOUR PREVIOUS CONFIG */ };
+const db = getFirestore(initializeApp(firebaseConfig));
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-document.getElementById('post-form').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const user = document.getElementById('username').value;
-    const msg = document.getElementById('message').value;
-    await addDoc(collection(db, "posts"), { user, msg, timestamp: new Date() });
-    document.getElementById('message').value = '';
-});
-
-const q = query(collection(db, "posts"), orderBy("timestamp", "asc"));
-onSnapshot(q, (snapshot) => {
-    const container = document.getElementById('forum-posts');
-    container.innerHTML = "";
-    snapshot.forEach((doc) => {
-        const p = doc.data();
-        container.innerHTML += `<div class="post"><h3>${p.user}</h3><p>${p.msg}</p></div>`;
+// Simple logic to add a thread
+document.querySelector('button').addEventListener('click', async () => {
+    const title = document.querySelector('input').value;
+    const content = document.querySelector('textarea').value;
+    
+    await addDoc(collection(db, "posts"), {
+        title, content, timestamp: new Date()
     });
+    alert("Thread Created!");
 });
